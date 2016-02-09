62da791b3c39f04818b74905fc390801d986cbcc
samiji <samijilani@live.com>
2016-02-09 20:12:13 +0100

    Supprimer chauffeur

--- backend/chauffeur.php
+++ backend/chauffeur.php
@@ -93,13 +93,13 @@ if (!isset($_SESSION['backend']))
                                         <h5><a href="ajoutchauffeur.php" class="btn btn-primary">Ajouter un chauffeur</a></h5>
                                     </header>
                                     <div id="collapse4" class="body">
-									     <?php
-                                    if (isset($_GET['id'])) {
-                                        mysql_query("delete from chauffeur where id_chauffeur=" . $_GET['id']);
-                                        echo "<script>alert('Chauffeur supprimé !')</script>";
-                                        echo "<script>window.location='chauffeur.php'</script>";
-                                    }
-                                    ?>
+                                        <?php
+                                        if (isset($_GET['id'])) {
+                                            mysql_query("delete from chauffeur where id_chauffeur=" . $_GET['id']);
+                                            echo "<script>alert('Chauffeur supprimé !')</script>";
+                                            echo "<script>window.location='chauffeur.php'</script>";
+                                        }
+                                        ?>
                                         <table id="dataTable" class="table table-bordered table-condensed table-hover table-striped">
                                             <thead>
                                                 <tr>
@@ -110,7 +110,7 @@ if (!isset($_SESSION['backend']))
                                                     <th>Tel</th>
                                                     <th>Véhicule</th>    
                                                     <th>Détails</th>
-													<th>Supprimer</th>
+                                                    <th>Supprimer</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -119,14 +119,14 @@ if (!isset($_SESSION['backend']))
                                                 while ($data = mysql_fetch_array($sql)) {
                                                     ?>
                                                     <tr>
-                                                        
+
                                                         <td><?php echo $data['prenom'] . " " . $data['nom']; ?></td>
                                                         <td><?php echo $data['email']; ?></td>
                                                         <td><?php echo $data['adresse']; ?></td>
                                                         <td><?php echo $data['tel']; ?></td>
                                                         <td><?php echo $data['typevehicule']; ?></td>            
                                                         <td><a href="detailchauffeur.php?id=<?php echo $data['id_chauffeur']; ?>" class="btn btn-warning btn-xs">Détails</a></td>
-														<td><a href="chauffeur.php?id=<?php echo $data['id_chauffeur']; ?>" class="btn btn-danger btn-xs">Supprimer</a></td>
+                                                        <td><a href="chauffeur.php?id=<?php echo $data['id_chauffeur']; ?>" class="btn btn-danger btn-xs">Supprimer</a></td>
                                                     </tr>
                                                     <?php
                                                 }
@@ -143,89 +143,15 @@ if (!isset($_SESSION['backend']))
                     </div><!-- /.inner -->
                 </div><!-- /.outer -->
             </div><!-- /#content -->
-            <div id="right" class="bg-light lter">
-                <div class="alert alert-danger">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong>Warning!</strong>  Best check yo self, you're not looking too good.
-                </div>
 
-                <!-- .well well-small -->
-                <div class="well well-small dark">
-                    <ul class="list-unstyled">
-                        <li>Visitor <span class="inlinesparkline pull-right">1,4,4,7,5,9,10</span> 
-                        </li>
-                        <li>Online Visitor <span class="dynamicsparkline pull-right">Loading..</span> 
-                        </li>
-                        <li>Popularity <span class="dynamicbar pull-right">Loading..</span> 
-                        </li>
-                        <li>New Users <span class="inlinebar pull-right">1,3,4,5,3,5</span> 
-                        </li>
-                    </ul>
-                </div><!-- /.well well-small -->
-
-                <!-- .well well-small -->
-                <div class="well well-small dark">
-                    <button class="btn btn-block">Default</button>
-                    <button class="btn btn-primary btn-block">Primary</button>
-                    <button class="btn btn-info btn-block">Info</button>
-                    <button class="btn btn-success btn-block">Success</button>
-                    <button class="btn btn-danger btn-block">Danger</button>
-                    <button class="btn btn-warning btn-block">Warning</button>
-                    <button class="btn btn-inverse btn-block">Inverse</button>
-                    <button class="btn btn-metis-1 btn-block">btn-metis-1</button>
-                    <button class="btn btn-metis-2 btn-block">btn-metis-2</button>
-                    <button class="btn btn-metis-3 btn-block">btn-metis-3</button>
-                    <button class="btn btn-metis-4 btn-block">btn-metis-4</button>
-                    <button class="btn btn-metis-5 btn-block">btn-metis-5</button>
-                    <button class="btn btn-metis-6 btn-block">btn-metis-6</button>
-                </div><!-- /.well well-small -->
-
-                <!-- .well well-small -->
-                <div class="well well-small dark">
-                    <span>Default</span> <span class="pull-right"><small>20%</small> </span> 
-                    <div class="progress xs">
-                        <div class="progress-bar progress-bar-info" style="width: 20%"></div>
-                    </div>
-                    <span>Success</span> <span class="pull-right"><small>40%</small> </span> 
-                    <div class="progress xs">
-                        <div class="progress-bar progress-bar-success" style="width: 40%"></div>
-                    </div>
-                    <span>warning</span> <span class="pull-right"><small>60%</small> </span> 
-                    <div class="progress xs">
-                        <div class="progress-bar progress-bar-warning" style="width: 60%"></div>
-                    </div>
-                    <span>Danger</span> <span class="pull-right"><small>80%</small> </span> 
-                    <div class="progress xs">
-                        <div class="progress-bar progress-bar-danger" style="width: 80%"></div>
-                    </div>
-                </div>
-            </div><!-- /#right -->
         </div><!-- /#wrap -->
         <footer class="Footer bg-dark dker">
-            <p>2015 &copy; Ad.prestiges</p>
+            <p>2016 &copy; ReseserverUnCab</p>
         </footer><!-- /#footer -->
 
         <!-- #helpModal -->
-        <div id="helpModal" class="modal fade">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title">Modal title</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal --><!-- /#helpModal -->
-	
+
+
 
         <!--jQuery -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
