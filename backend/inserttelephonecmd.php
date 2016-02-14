7753f2869b901c8cf147c7f9477d4d292e46be00
samiji <samijilani@live.com>
2016-02-14 21:23:14 +0100

    reservation par tel

--- backend/inserttelephonecmd.php
+++ backend/inserttelephonecmd.php
@@ -28,7 +28,7 @@ $note = $_GET['note'];
 
 //Ajout du client à la base si il n'a jamais command� par tel
 if ($nom != "" or $tel != "" or $prenom != "") {
-    
+
     $new_client = mysql_fetch_array(mysql_query("insert into client_tel (nom, prenom, email, tel) values('" . $nom . "','" . $prenom . "','" . $email . "','" . $tel . "')"));
 
     $client = mysql_insert_id();
@@ -40,39 +40,60 @@ if ($nom != "" or $tel != "" or $prenom != "") {
 $nom_chauffeur = mysql_fetch_array(mysql_query("select * from chauffeur where id_chauffeur =" . $chauffeur));
 $nom_complet = $nom_chauffeur['prenom'] . " " . $nom_chauffeur['nom'];
 
-$headers = 'From: contact@reserveruncab.com' . "\r\n" .
-        'Reply-To: contact@reserveruncab.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-$message = "Notification de réservation :\n
 
-
-Nom : " . $nom . "\n
-Prénom : " . $prenom . "\n
-Téléphone: " . $tel . "\n
-Départ : " . $depart . "\n
-Arrivée : " . $arrivee . "\n
-Prix : " . $prix . "\n
-Date : " . $date . "\n
-Heure : " . $heure . "\n
-";
-mail($nom_chauffeur['email'], "Notification sur ReserverUnCab.com", $message, $headers);
 
 if ($client != "" or $client > 0) {
-    
-    $sql = mysql_query("INSERT INTO `reserverrzad`.`reservation_tel` ( `depart`, `arrivee`, `tel`, `prix`, `part_chauffeur`, `part_societe`, `dtdeb`, `date_add`, `heure`, `societe`, `siren`, `id_chauffeur`, `passager`, `valise`, `type_vehicule`, `client`, `etat`, `notif`, `archive`, `paiement`) VALUES ( '".$depart."', '".$arrivee."', '".$tel."', '".$prix."', '".$part_chauffeur."', '".$part_societe."', '".$dtdeb."', '". date('Y-m-d')."', '".$heure."', '".$societe."', '".$siren."', ".$chauffeur.", ".$passager.", ".$valise.", ".$type_vehicule.", '".$client."', 0, 1, 0, '".$paiement."');") or die(mysql_error());
-  $id_cmd=  mysql_insert_id();
-    
+
+    $sql = mysql_query("INSERT INTO `reserverrzad`.`reservation_tel` ( `depart`, `arrivee`, `tel`, `prix`, `part_chauffeur`, `part_societe`, `dtdeb`, `date_add`, `heure`, `societe`, `siren`, `id_chauffeur`, `passager`, `valise`, `type_vehicule`, `client`, `etat`, `notif`, `archive`, `paiement`) VALUES ( '" . $depart . "', '" . $arrivee . "', '" . $tel . "', '" . $prix . "', '" . $part_chauffeur . "', '" . $part_societe . "', '" . $dtdeb . "', '" . date('Y-m-d') . "', '" . $heure . "', '" . $societe . "', '" . $siren . "', " . $chauffeur . ", " . $passager . ", " . $valise . ", " . $type_vehicule . ", '" . $client . "', 0, 1, 0, '" . $paiement . "');") or die(mysql_error());
+    $id_cmd = mysql_insert_id();
+    $ll = mysql_query("select client_tel.nom,client_tel.prenom,client_tel.type_user,client_tel.tel, reservation_tel.depart,reservation_tel.arrivee,reservation_tel.id as id_cmd,reservation_tel.dtdeb,reservation_tel.prix,reservation_tel.dtdeb,client_tel.email,reservation_tel.heure,reservation_tel.passager,reservation_tel.valise from client_tel inner join reservation_tel on reservation_tel.client = client_tel.id where  reservation_tel.id=" . $id_cmd . " order by reservation_tel.id desc limit 1")or die(mysql_error());
+    $commande = mysql_fetch_array($ll);
+
+    $headers = 'From: contact@reserveruncab.com' . "\r\n" .
+            'Reply-To: contact@reserveruncab.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+    $message = "Notification de réservation :\n
+Client : " . $commande['type_user'] . ": " . $commande['prenom'] . " " . $commande['nom'] . "\n
+Téléphone : " . $commande['tel'] . "\n
+Date : " . $commande['dtdeb'] . "\n
+Heure : " . $commande['heure'] . "\n
+Départ : " . $commande['depart'] . "\n
+Arrivée : " . $commande['arrivee'] . "\n
+Prix : " . $commande['prix'] . "\n
+Passagers : " . $commande['passager'] . "\n
+Valises : " . $commande['valise'] . "\n
+    ";
+    mail($nom_chauffeur['email'], "Notification sur ReserverUnCab.com", $message, $headers);
 } else {
-    
+
     $sql = mysql_query("Insert into reservation_tel(id_chauffeur, tel, part_societe ,part_chauffeur, depart, arrivee, prix, date_add, dtdeb, heure, valise, passager, client, siren, societe, type_vehicule, etat, notif, archive, paiement, ,note) values('" . $chauffeur . "','" . $tel . "', '" . $part_societe . "', '" . $part_chauffeur . "' , '" . $depart . "', '" . $arrivee . "', '" . $prix . "','" . date('d-m-Y') . "', '" . $dtdeb . "', '" . $heure . "', '" . $valise . "', '" . $passager . "',0,'" . $siren . "','" . $societe . "','" . $type_vehicule . "', 0, 1, 0,'" . $paiement . "','" . $note . "';")or die(mysql_error());
-  $id_cmd=  mysql_insert_id();
+    $id_cmd = mysql_insert_id();
     $sql2 = mysql_query("Insert into client_tel(nom, prenom, tel, email) values('" . $nom . "','" . $prenom . "','" . $tel . "','" . $email . "';")or die(mysql_error());
+    $ll = mysql_query("select client_tel.nom,client_tel.prenom,client_tel.type_user,client_tel.tel, reservation_tel.depart,reservation_tel.arrivee,reservation_tel.id as id_cmd,reservation_tel.dtdeb,reservation_tel.prix,reservation_tel.dtdeb,client_tel.email,reservation_tel.heure,reservation_tel.passager,reservation_tel.valise from client_tel inner join reservation_tel on reservation_tel.client = client_tel.id where  reservation_tel.id=" . $id_cmd . " order by reservation_tel.id desc limit 1")or die(mysql_error());
+    $commande = mysql_fetch_array($ll);
+
+    $headers = 'From: contact@reserveruncab.com' . "\r\n" .
+            'Reply-To: contact@reserveruncab.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+    $message = "Notification de réservation :\n
+Client : " . $commande['type_user'] . ": " . $commande['prenom'] . " " . $commande['nom'] . "\n
+Téléphone : " . $commande['tel'] . "\n
+Date : " . $commande['dtdeb'] . "\n
+Heure : " . $commande['heure'] . "\n
+Départ : " . $commande['depart'] . "\n
+Arrivée : " . $commande['arrivee'] . "\n
+Prix : " . $commande['prix'] . "\n
+Passagers : " . $commande['passager'] . "\n
+Valises : " . $commande['valise'] . "\n
+    ";
+    mail($nom_chauffeur['email'], "Notification sur ReserverUnCab.com", $message, $headers);
 }
+
 
 
 $rs = "[";
 
-$rs.="{id:'".$id_cmd."'},";
+$rs.="{id:'" . $id_cmd . "'},";
 
 $rs.="]";
 
--- paiementValideTel.php
+++ paiementValideTel.php
@@ -1,12 +1,85 @@
 <?php
 include("config.php");
- 
 require 'phpmailer/class.phpmailer.php';
 
 $id_cmd = $_GET['id'];
 //$user = mysql_fetch_array(mysql_query("select * from client_tel where email='" . $_SESSION['myvtclogin'] . "'"));
-$ll = mysql_query("select reservation_tel.depart,reservation_tel.arrivee,reservation_tel.id as id_cmd,DATE_FORMAT(reservation_tel.dtdeb,'%d/%m/%Y') as datereservation,reservation_tel.prix,reservation_tel.dtdeb,client_tel.email,reservation_tel.heure,reservation_tel.passager,reservation_tel.valise from client_tel inner join reservation_tel on reservation_tel.client = client_tel.id where  reservation_tel.id=" . $_GET['id'] . " order by reservation_tel.id desc limit 1")or die(mysql_error());
+$ll = mysql_query("select reservation_tel.depart,reservation_tel.arrivee,reservation_tel.id as id_cmd,DATE_FORMAT(reservation_tel.dtdeb,'%d/%m/%Y') as datereservation,reservation_tel.prix,reservation_tel.dtdeb,client_tel.email,client_tel.id as id_clt,reservation_tel.heure,reservation_tel.passager,reservation_tel.valise from client_tel inner join reservation_tel on reservation_tel.client = client_tel.id where  reservation_tel.id=" . $_GET['id'] . " order by reservation_tel.id desc limit 1")or die(mysql_error());
 $commande = mysql_fetch_array($ll);
+$user = mysql_fetch_array(mysql_query("select * from myvtc_users where email='" .$commande['email']. "'")or die(mysql_error()));
+
+
+// reference the Dompdf namespace
+ /*use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$chaine = utf8_encode('<table border="0" style="width:100%">
+            <tr>
+                <td colspan="2" style="text-align: center"><img src="http://www.reserveruncab.com/images/logo.png" alt="" /></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center"><h2>Facture N&deg; ' . $commande['id_cmd'] . '</h2></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: left"><br><br><br><br>Bonjour ' . $user["prenom"] . ',<br><br>Ci-dessous, vous trouvez le d&eacute;tail de votre commande : <br><br><br></td>
+            </tr>
+             <tr>
+                 <td colspan="2" >
+                     <table border="1" style="width:600px">
+                         <tr>
+                             <td style="height:25px; width:200px">D&eacute;part</td>
+                             <td> ' . $commande['depart'] . '</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Arriv&eacute;</td>
+                             <td> ' . $commande['arrivee'] . '</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Date</td>
+                             <td> ' . $commande['datereservation'] . '</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Heure</td>
+                             <td> ' . $commande['heure'] . '</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Prix</td>
+                             <td> ' . $commande['prix'] . ' €</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Passagers</td>
+                             <td> ' . $commande['passager'] . ' €</td>
+                         </tr>
+                         <tr style="height:35px">
+                             <td style="height:25px">Valises</td>
+                             <td> ' . $commande['valise'] . ' €</td>
+                         </tr>
+                     </table>
+                 </td>
+               
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center; padding-top:200px">L\'&eacute;quipe ReserverUnCab.com</td>
+            </tr>
+        </table>');
+$dompdf->loadHtml($chaine, 'UTF-8');
+
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'paysage');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Get the generated PDF file contents
+//$pdf = $dompdf->output();
+file_put_contents('Facture_' . $commande["id_cmd"] . '.pdf', $dompdf->output());*/
+
+
+
+
+
 
 $mail = new PHPMailer;
 
@@ -21,10 +94,10 @@ $mail->Password = 'Balloo94';                  // SMTP password
 
 $mail->From = 'contact@reserveruncab.com';
 $mail->FromName = 'ReserverUnCab';
-$mail->AddAddress($_SESSION['myvtclogin'], $_SESSION['myvtclogin']); // Add address
+$mail->AddAddress($commande['email'], $commande['email']); // Add address
 
 
-$mail->Subject = 'Validation de paiement ReserverUnCab.com';
+$mail->Subject = 'Validation de reservation ReserverUnCab.com';
 $mail->Body    = "Bonjour,<br>
 
 Fécilitation ! Votre réservation sur le site ReserverUnCab.com a été effectué avec succès.<br><br>
@@ -40,7 +113,7 @@ Voici le détail de votre commande :<br><br>
 L'équipe ReserverUnCab.com.";
 
 
-$mail->AddAttachment("Facture_" . $commande['id_cmd'] . ".pdf");  
+//$mail->AddAttachment("Facture_" . $commande['id_cmd'] . ".pdf");  
    // Pour finir, on envoi l'e-mail
    $mail->send();
 
